1bf35e33fd6b919e480080061b770dafb6a09257
Matthew Rocklin <mrocklin@gmail.com>
2013-01-10 18:56:05 -0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from os.path import exists
+from setuptools import setup
+
+setup(name='logpy',
+      version='0.1',
+      description='Logic Programming in python',
+      url='http://github.com/logpy/logpy',
+      author='Matthew Rocklin',
+      author_email='mrocklin@gmail.com',
+      license='BSD',
+      packages=['logpy'],
+      long_description=open('README.md').read() if exists("README.md") else "",
+      zip_safe=False)
