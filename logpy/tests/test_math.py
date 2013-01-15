b27090a04354659d831cb55afd7f4a3c5f5a3148
Matthew Rocklin <mrocklin@gmail.com>
2013-01-15 09:41:56 -0800

    primo goalevals by default

--- logpy/tests/test_math.py
+++ logpy/tests/test_math.py
@@ -7,3 +7,5 @@ def test_primo():
     assert set(run(0, x, membero(x, (1,2,3,4,5,6,7,8,9,10,11)),
                            (primo, x))) == set((2, 3, 5, 7, 11))
     assert all(isprime(i) for i in run(5, x, primo(x)))
+    assert set(run(0, x, membero(x, (1,2,3,4,5,6,7,8,9,10)), primo(x))) == \
+            set((2, 3, 5, 7))
