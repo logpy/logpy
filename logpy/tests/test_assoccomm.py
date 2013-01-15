06200d8b8bb3d7f4ecdf01eaf8a184645aab2949
Matthew Rocklin <mrocklin@gmail.com>
2013-01-15 12:33:55 -0800

    correct spacing between \d,\d

--- logpy/tests/test_assoccomm.py
+++ logpy/tests/test_assoccomm.py
@@ -4,20 +4,23 @@ from logpy.assoccomm import unify_assoc, unify_comm
 def test_assoc():
     add = 'add'
     x = var()
-    assert tuple(unify_assoc((add, 1,2,3), (add, 1,2,3), {}))
-    assert tuple(unify_assoc((add, 1,2,3), (add, (add, 1, 2), 3), {}))
-    assert tuple(unify_assoc((add, 1,2,3), (add, 1,x,3), {})) == ({x: 2},)
-    assert tuple(unify_assoc((add, 1,2,3), (add, 1, x), {})) == ({x: (add, 2, 3)},)
-    assert tuple(unify_assoc((add, 1,2,3), (add, x, 3), {})) == ({x: (add, 1, 2)},)
-    assert tuple(unify_assoc((add, x, 3), (add, 1,2,3), {})) == ({x: (add, 1, 2)},)
+    assert tuple(unify_assoc((add, 1, 2, 3), (add, 1, 2, 3), {}))
+    assert tuple(unify_assoc((add, 1, 2, 3), (add, (add, 1, 2), 3), {}))
+    assert tuple(unify_assoc((add, 1, 2, 3), (add, 1,x,3), {})) == ({x: 2},)
+    assert tuple(unify_assoc((add, 1, 2, 3), (add, 1, x), {})) == \
+                                 ({x: (add, 2, 3)},)
+    assert tuple(unify_assoc((add, 1, 2, 3), (add, x, 3), {})) == \
+                                 ({x: (add, 1, 2)},)
+    assert tuple(unify_assoc((add, x, 3), (add, 1, 2, 3), {})) == \
+                                 ({x: (add, 1, 2)},)
 
 def test_comm():
     add = 'add'
     x = var()
-    assert tuple(unify_comm((add, 1,2,3), (add, 1,2,3), {}))
-    assert tuple(unify_comm((add, 3,2,1), (add, 1,2,3), {}))
-    assert tuple(unify_comm((add, 1,2,3), (add, 1, x), {})) == \
-                            ({x: (add, 2, 3)}, {x: (add, 3, 2)})
+    assert tuple(unify_comm((add, 1, 2, 3), (add, 1, 2, 3), {}))
+    assert tuple(unify_comm((add, 3, 2, 1), (add, 1, 2, 3), {}))
+    assert set(unify_comm((add, 1, 2, 3), (add, 1, x), {})) == \
+                        set(({x: (add, 2, 3)}, {x: (add, 3, 2)}))
     assert tuple(unify_comm((add, (add, 3, 1), 2), (add, 1, 2, 3), {}))
 
 """
