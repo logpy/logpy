23e4917b8f6f604eb3a7e94da6afce0e6c8becc7
Matthew Rocklin <mrocklin@gmail.com>
2013-01-18 20:03:51 -0800

    remove pair

--- logpy/core.py
+++ logpy/core.py
@@ -271,29 +271,6 @@ def facts(rel, *lists):
     for l in lists:
         fact(rel, *l)
 
-class pair(tuple):
-    def __new__(cls, head, tail):
-        obj = tuple.__new__(cls)
-        obj.head = head
-        obj.tail = tail
-        return obj
-
-    def __getitem__(self, key):
-        if key == 0:
-            return self.head
-        raise NotImplementedError()
-
-    def __getslice__(self, a, b):
-        if a == 1 and b > 100:
-            return self.tail
-        raise NotImplementedError()
-
-    def __iter__(self):
-        yield self.head
-        for i in self.tail:
-            yield i
-
-
 def conso(h, t, l):
     if isinstance(l, tuple):
         if len(l) == 0:
