c5c68cdfc92f64e5f6e60be32811b77484fcdc3c
Matthew Rocklin <mrocklin@gmail.com>
2013-01-18 17:41:24 -0800

    Refactor bindearly to generic bind strategies

--- logpy/strategies.py
+++ logpy/strategies.py
@@ -3,16 +3,30 @@ from logpy.core import goaleval, EarlyGoalError
 
 
 def bindearly(stream, *goals):
-    return interleave(earlysafe(s, goals) for s in stream)
+    """ Bind* that intelligently handles EarlyGoalErrors """
+    return bindstarstrat(stream, goals, [earlysafe], interleave)
 
-def earlysafe(s, goals):
+def bindstarstrat(stream, goals, strats, joiner=interleave):
+    """ Strategic bind* """
+    return joiner(bindstrat(s, goals, strats) for s in stream)
+
+def bindstrat(s, goals, strats, joiner=interleave):
+    """ Strategic bind """
     if not goals:
         return (s,)
+    for strat in strats:
+        goals = strat(s, goals)
+    stream = goaleval(goals[0])(s)
+    return bindstarstrat(stream, goals[1:], strats, joiner)
+
+def earlysafe(s, goals):
+    """ Place first goal at end if it raises an EarlyGoalError """
+    if not goals:
+        return goals
     goal = goaleval(goals[0])
     try:
-        substream = goal(s)
-        return bindearly(substream, *goals[1:])
+        goal(s)
+        return goals
     except EarlyGoalError:
         regoals = goals[1:] + (goals[0],)
         return earlysafe(s, regoals)
-
