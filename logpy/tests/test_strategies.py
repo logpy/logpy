0e564eef2036f597c2a1c170908393b6c508589b
Matthew Rocklin <mrocklin@gmail.com>
2013-01-22 12:04:18 -0800

    conso early test

--- logpy/tests/test_strategies.py
+++ logpy/tests/test_strategies.py
@@ -1,14 +1,19 @@
-from logpy.core import var, run, eq, membero, fail
+from logpy.core import var, run, eq, membero, fail, conso
 from logpy.strategies import bindearly, anyfail
 
+x = var()
+y = var()
+z = var()
+
 def test_early():
-    x = var()
-    y = var()
     assert run(0, x, (eq, y, (1, 2)), (membero, x, y), bindfn=bindearly)
     assert run(0, x, (membero, x, y), (eq, y, (1, 2)), bindfn=bindearly)
 
 def test_fail():
-    x = var()
     def failconstructor():
         return fail
     assert tuple(anyfail({}, ((eq, x, 1), (failconstructor,)))) == (fail,)
+
+def test_conso_early():
+    assert (run(0, x, (conso, x, y, z), (eq, z, (1, 2, 3)), bindfn=bindearly)
+            == (1,))
