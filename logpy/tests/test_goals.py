591d4571d42159f91b37fce835d64efb9c54ddf2
Matthew Rocklin <mrocklin@gmail.com>
2013-03-14 20:19:19 -0700

    remove setaddo
    This was unused and not particularly well written

--- logpy/tests/test_goals.py
+++ logpy/tests/test_goals.py
@@ -1,4 +1,4 @@
-from logpy.goals import tailo, heado, appendo, seteq, conso, setaddo
+from logpy.goals import tailo, heado, appendo, seteq, conso
 from logpy.core import var, run, eq, EarlyGoalError, goaleval
 from logpy.util import raises
 
@@ -45,20 +45,6 @@ def test_conso_early():
     x, y, z = var(), var(), var()
     assert (run(0, x, (conso, x, y, z), (eq, z, (1, 2, 3)))
             == (1,))
-
-def test_setaddo():
-    x = var('x')
-    y = var('y')
-    assert results(setaddo(1, (2, 3), (1, 2, 3)))
-    assert results(setaddo(1, (2, 3), (1, 3, 2)))
-    assert results(setaddo(1, (2, 3), (2, 3, 1)))
-    assert not results(setaddo(1, (2, 3), (2, 3)))
-    assert results(setaddo(1, x, (2, 3, 1)))
-    assert not results(setaddo(4, x, (2, 3, 1)))
-    assert len(results(setaddo(y, x, (2, 3, 1)))) in (3, 6)
-    assert set(run(0, x, setaddo(1, x, (2, 3, 1)))) == set([(2, 3), (3, 2)])
-
-    assert set(run(1, x, setaddo(1, (2, 3), x))[0]) == set((1, 2, 3))
 
 def test_appendo():
     x = var('x')
