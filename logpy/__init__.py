cf211e93e336b0c605605afe54c0ea4f70162ae5
Matthew Rocklin <mrocklin@gmail.com>
2013-03-26 18:39:29 -0500

    import isvar in __init__.py

--- logpy/__init__.py
+++ logpy/__init__.py
@@ -2,7 +2,7 @@
 LogPy is a Python library for logic and relational programming.
 """
 
-from core import run, eq, var, conde, membero
+from core import run, eq, var, isvar, conde, membero
 from goals import seteq
 from facts import Relation, fact, facts
 
