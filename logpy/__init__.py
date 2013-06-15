9ef4671e9dc3c3bd44284cf25fd090581bbba6cb
Matthew Rocklin <mrocklin@gmail.com>
2013-06-15 16:06:29 -0500

    permuteq in __init__

--- logpy/__init__.py
+++ logpy/__init__.py
@@ -4,7 +4,7 @@ LogPy is a Python library for logic and relational programming.
 
 from core import run, eq, conde, membero
 from variable import var, isvar, variables, Var
-from goals import seteq, goalify
+from goals import seteq, permuteq, goalify
 from facts import Relation, fact, facts
 from unification import unify, reify
 from unifymore import logify
