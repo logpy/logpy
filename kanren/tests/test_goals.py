logpy/logpy

commit 4cc6660be4a6be0bdd85900baf344d293b4948c7

Add tests for core methods and rename lall implementations.
The `lall` method in logpy.core could raise preventable EarlyExitError
if the caller didn't order the goals correctly. The old implementation of
lall has been renamed to lallgreedy (to reflect that it's a performance
optimization).

Note that the implementation of `earlyorder` is order preserving _if_
no goal raises an EarlyGoalError, so rename does not change the behavior
of `lall` in cases where goals do not need to be reordered.

There is some negative performance impact. However, the usability gain is
significant, and the old behavior is still available in lallgreedy if needed
for performance reasons.

logpy/assoccomm.py
@@ -30,8 +30,8 @@ be used in the computer algebra systems SymPy and Theano.
 """
 
 from logpy.core import (isvar, assoc, unify,
-        conde, var, eq, fail, goaleval, lall, EarlyGoalError,
-        condeseq, goaleval)
+                        conde, var, eq, fail, goaleval, lallgreedy, EarlyGoalError,
+                        condeseq, goaleval)
 from .goals import heado, permuteq, conso, tailo
 from .facts import Relation
 from logpy import core
@@ -202,7 +202,8 @@ def buildo(op, args, obj):
     """
     if not isvar(obj):
         oop, oargs = op_args(obj)
-        return lall((eq, op, oop), (eq, args, oargs))
+        # TODO: Is greedy correct?
+        return lallgreedy((eq, op, oop), (eq, args, oargs))
     else:
         try:
             return eq(obj, build(op, args))
@@ -274,5 +275,6 @@ def eq_assoccomm(u, v):
     if vop and not uop:
         u, v = v, u
     w = var()
-    return (lall, (eq_assoc, u, w, eq_assoccomm, n),
-                  (eq_comm, v, w, eq_assoccomm))
+    # TODO: Is greedy correct?
+    return (lallgreedy, (eq_assoc, u, w, eq_assoccomm, n),
+            (eq_comm, v, w, eq_assoccomm))

logpy/core.py
@@ -45,13 +45,34 @@ def membero(x, coll):
 ################################
 
 def lall(*goals):
-    """ Logical all
+    """ Logical all with goal reordering to avoid EarlyGoalErrors
+
+    See also:
+        EarlyGoalError
+        earlyorder
 
     >>> from logpy.core import lall, membero
     >>> x = var('x')
-    >>> g = lall(membero(x, (1,2,3)), membero(x, (2,3,4)))
-    >>> tuple(g({}))
-    ({~x: 2}, {~x: 3})
+    >>> run(0, x, lall(membero(x, (1,2,3)), membero(x, (2,3,4))))
+    (2, 3)
+    """
+    return (lallgreedy,) + tuple(earlyorder(*goals))
+
+def lallgreedy(*goals):
+    """ Logical all that greedily evaluates each goals in the order provided.
+
+    Note that this may raise EarlyGoalError when the ordering of the
+    goals is incorrect. It is faster than lall, but should be used
+    with care.
+
+    >>> from logpy.core import lallgreedy, eq, run
+    >>> x, y = var('x'), var('y')
+    >>> run(0, x, lallgreedy((eq, y, set([1]))), (membero, x, y))
+    (1,)
+    >>> run(0, x, lallgreedy((membero, x, y), (eq, y, set([1]))))
+    Traceback (most recent call last):
+      ...
+    EarlyGoalError
     """
     if not goals:
         return success
@@ -60,7 +81,7 @@ def lall(*goals):
     def allgoal(s):
         g = goaleval(reify(goals[0], s))
         return unique(interleave(
-                        goaleval(reify((lall,) + tuple(goals[1:]), ss))(ss)
+                        goaleval(reify((lallgreedy,) + tuple(goals[1:]), ss))(ss)
                         for ss in g(s)),
                       key=dicthash)
     return allgoal
@@ -68,11 +89,13 @@ def lall(*goals):
 def lallfirst(*goals):
     """ Logical all - Run goals one at a time
 
-    >>> from logpy.core import lall, membero
+    >>> from logpy.core import lallfirst, membero
     >>> x = var('x')
-    >>> g = lall(membero(x, (1,2,3)), membero(x, (2,3,4)))
+    >>> g = lallfirst(membero(x, (1,2,3)), membero(x, (2,3,4)))
     >>> tuple(g({}))
     ({~x: 2}, {~x: 3})
+    >>> tuple(lallfirst()({}))
+    ({},)
     """
     if not goals:
         return success
@@ -105,15 +128,6 @@ def lany(*goals):
         return goals[0]
     return lanyseq(goals)
 
-def lallearly(*goals):
-    """ Logical all with goal reordering to avoid EarlyGoalErrors
-
-    See also:
-        EarlyGoalError
-        earlyorder
-    """
-    return (lall,) + tuple(earlyorder(*goals))
-
 def earlysafe(goal):
     """ Call goal be evaluated without raising an EarlyGoalError """
     try:
@@ -126,11 +140,13 @@ def earlyorder(*goals):
     """ Reorder goals to avoid EarlyGoalErrors
 
     All goals are evaluated.  Those that raise EarlyGoalErrors are placed at
-    the end in a lallearly
+    the end in a lall
 
     See also:
         EarlyGoalError
     """
+    if not goals:
+        return ()
     groups = groupby(earlysafe, goals)
     good = groups.get(True, [])
     bad  = groups.get(False, [])
@@ -140,20 +156,21 @@ def earlyorder(*goals):
     elif not bad:
         return tuple(good)
     else:
-        return tuple(good) + ((lallearly,) + tuple(bad),)
+        return tuple(good) + ((lall,) + tuple(bad),)
 
-def conde(*goalseqs, **kwargs):
+def conde(*goalseqs):
     """ Logical cond
 
     Goal constructor to provides logical AND and OR
 
     conde((A, B, C), (D, E)) means (A and B and C) or (D and E)
+    Equivalent to the (A, B, C); (D, E) in Prolog.
 
     See Also:
         lall - logical all
         lany - logical any
     """
-    return (lany, ) + tuple((lallearly,) + tuple(gs) for gs in goalseqs)
+    return (lany, ) + tuple((lall,) + tuple(gs) for gs in goalseqs)
 
 
 def lanyseq(goals):
@@ -179,7 +196,7 @@ def lanyseq(goals):
 
 def condeseq(goalseqs):
     """ Like conde but supports generic (possibly infinite) iterator of goals"""
-    return (lanyseq, ((lallearly,) + tuple(gs) for gs in goalseqs))
+    return (lanyseq, ((lall,) + tuple(gs) for gs in goalseqs))
 
 ########################
 # User level execution #
@@ -199,7 +216,7 @@ def run(n, x, *goals, **kwargs):
     >>> run(1, x, eq(x, 1))
     (1,)
     """
-    results = map(partial(reify, x), goaleval(lallearly(*goals))({}))
+    results = map(partial(reify, x), goaleval(lall(*goals))({}))
     return take(n, unique(results, key=multihash))
 
 ###################
@@ -219,7 +236,7 @@ class EarlyGoalError(Exception):
     collections.  This is unproductive.  Rather than proceed, membero raises an
     EarlyGoalError, stating that this goal has been called early.
 
-    The goal constructor lallearly Logical-All-Early will reorder such goals to
+    The goal constructor lall Logical-All-Early will reorder such goals to
     the end so that the call becomes
 
     >>> run(0, x, (eq, coll, (1, 2, 3)), (membero, x, coll)) # doctest: +SKIP
@@ -228,7 +245,7 @@ class EarlyGoalError(Exception):
     all elements of coll, 1, then 2, then 3.
 
     See Also:
-        lallearly
+        lall
         earlyorder
     """
 

logpy/goals.py
@@ -1,5 +1,5 @@
-from .core import (var, isvar, eq, EarlyGoalError, conde, condeseq, lany, lall,
-        lallearly, fail, success)
+from .core import (var, isvar, eq, EarlyGoalError, conde, condeseq, lany, lallgreedy,
+                   lall, fail, success)
 from .util import unique
 import itertools as it
 
@@ -147,5 +147,5 @@ isinstanceo = goalify(isinstance)
 def appendo(l, s, ls):
     """ Byrd thesis pg. 247 """
     a, d, res = [var() for i in range(3)]
-    return (lany, (lall, (eq, l, ()), (eq, s, ls)),
-                  (lallearly, (conso, a, d, l), (conso, a, res, ls), (appendo, d, s, res)))
+    return (lany, (lallgreedy, (eq, l, ()), (eq, s, ls)),
+                  (lall, (conso, a, d, l), (conso, a, res, ls), (appendo, d, s, res)))

logpy/tests/test_core.py
@@ -1,9 +1,11 @@
+import pytest
 from pytest import raises
 
 from logpy.core import (walk, walkstar, var, run,
-        membero, evalt, fail, eq, conde,
-        goaleval, lany, lall, lanyseq,
-        goalexpand, earlyorder, EarlyGoalError, lallearly, earlysafe)
+                        membero, evalt, fail, eq, conde,
+                        goaleval, lany, lallgreedy, lanyseq,
+                        goalexpand, earlyorder, EarlyGoalError, lall, earlysafe,
+                        lallfirst)
 
 w, x, y, z = 'wxyz'
 
@@ -29,10 +31,25 @@ def test_lany():
     assert len(tuple(lany(eq(x, 2), eq(x, 3))({}))) == 2
     assert len(tuple(lany((eq, x, 2), (eq, x, 3))({}))) == 2
 
-def test_lall():
-    x = var('x')
-    assert results(lall((eq, x, 2))) == ({x: 2},)
-    assert results(lall((eq, x, 2), (eq, x, 3))) == ()
+
+# Test that all three implementations of lallgreedy behave identically for
+# correctly ordered goals.
+@pytest.mark.parametrize('lall_impl', [lallgreedy, lall, lallfirst])
+def test_lall(lall_impl):
+    x, y = var('x'), var('y')
+    assert results(lall_impl((eq, x, 2))) == ({x: 2},)
+    assert results(lall_impl((eq, x, 2), (eq, x, 3))) == ()
+    assert results(lall_impl()) == ({},)
+
+    assert run(0, x, lall_impl((eq, y, (1, 2)), (membero, x, y)))
+    assert run(0, x, lall_impl()) == (x,)
+    with pytest.raises(EarlyGoalError):
+        run(0, x, lall_impl(membero(x, y)))
+
+@pytest.mark.parametrize('lall_impl', [lall, lallfirst])
+def test_safe_reordering_lall(lall_impl):
+    x, y = var('x'), var('y')
+    assert run(0, x, lall_impl((membero, x, y), (eq, y, (1, 2)))) == (1, 2)
 
 def test_earlysafe():
     x, y = var('x'), var('y')
@@ -89,6 +106,9 @@ def test_membero():
                          membero(x, (2,3,4)))) == set((2,3))
 
     assert run(5, x, membero(2, (1, x, 3))) == (2,)
+    assert run(0, x, (membero, 1, (1, 2, 3))) == (x, )
+    assert run(0, x, (membero, 1, (2, 3))) == ()
+
 
 def test_lanyseq():
     x = var('x')
@@ -113,6 +133,7 @@ def test_uneval_membero():
     assert set(run(100, x, (membero, y, ((1,2,3),(4,5,6))), (membero, x, y))) == \
            set((1,2,3,4,5,6))
 
+
 def test_goaleval():
     x, y = var('x'), var('y')
     g = eq(x, 2)
@@ -120,7 +141,7 @@ def test_goaleval():
     assert callable(goaleval((eq, x, 2)))
     with raises(EarlyGoalError):
         goaleval((membero, x, y))
-    assert callable(goaleval((lall, (eq, x, 2))))
+    assert callable(goaleval((lallgreedy, (eq, x, 2))))
 
 def test_goalexpand():
     def growing_goal(*args):
@@ -132,10 +153,6 @@ def test_goalexpand():
     g = (growing_goal, 2)
     assert goalexpand(g) == (growing_goal, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2)
 
-def test_early():
-    x, y = var(), var()
-    assert run(0, x, lallearly((eq, y, (1, 2)), (membero, x, y)))
-    assert run(0, x, lallearly((membero, x, y), (eq, y, (1, 2))))
 
 def test_lany_is_early_safe():
     x = var()

logpy/util.py
@@ -122,7 +122,7 @@ def pprint(g):
     """ Pretty print a tree of goals """
     if callable(g) and hasattr(g, '__name__'):
         return g.__name__
-    if isinstance(g, type):
+    if isinstance(g, type):  # pragma: no cover
         return g.__name__
     if isinstance(g, tuple):
         return "(" + ', '.join(map(pprint, g)) + ")"

commit 6c4a1b89cf3cd062da8405e0d1f62a790af64de2

Expand the test to exhibit was the issue introduced by 4cc6660be4a6be0bdd85900baf344d293b4948c7 was.

kanren/tests/test_goals.py
@@ -4,7 +4,7 @@ from unification import unify, var
 
 from ..goals import (tailo, heado, appendo, seteq, conso, typo,
                           isinstanceo, permuteq, LCons, membero)
-from ..core import run, eq, goaleval, lall
+from ..core import run, eq, goaleval, lall, lallgreedy
 
 x, y, z, w = var('x'), var('y'), var('z'), var('w')
 
@@ -144,11 +144,25 @@ def test_goal_ordering():
         return membero((q, p), zip(lst, lst[1:]))
 
     vals = var()
-    rules = (
+
+    # Verify the solution can be computed when we specify the execution
+    # ordering.
+    rules_greedy = (
+        lallgreedy,
+        (eq, (var(), var()), vals),
+        (lefto, 'green', 'white', vals),
+    )
+
+    solution, = run(1, vals, rules_greedy)
+    assert solution == ('green', 'white')
+
+    # Verify that attempting to compute the "safe" order does not itself cause
+    # the evaluation to fail.
+    rules_greedy = (
         lall,
         (eq, (var(), var()), vals),
         (lefto, 'green', 'white', vals),
     )
 
-    solution, = run(1, vals, rules)
+    solution, = run(1, vals, rules_greedy)
     assert solution == ('green', 'white')